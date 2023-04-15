7222bb00b73bc70a95218ac193913f5f3484e743
Kamok1 <kkamiss12@gmail.com>
2023-04-15 21:22:26 +0200

    fix

---
+++ EfExtensions/EfExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Exceptions;
+
+namespace EfExtensions
+{
+  public static class EfExtensions
+  {
+    public async static Task<T> FindOrThrowErrorAsync<T>(this DbSet<T> dbSet, int id) where T : class
+    {
+      return await dbSet.FindAsync(id) ?? throw new NotFoundException<T>();
+    }
+  }
+}
